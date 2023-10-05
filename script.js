b475a119f59e2c45ed85a405b27babe2d4de9d2a
TanmayVedpathak <tanmayvedpathak19@gmail.com>
2023-10-05 08:59:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+let reviewsBody = document.getElementById("reviewsBody");
+
+const getNameInitials = (name) => {
+    const splitName = name.split(" ")
+    return splitName[0][0].toUpperCase() + splitName[1][0].toUpperCase()
+}
+
+function getDateDifference(givenDate) {
+    const givenDateTime = new Date(givenDate);
+    const currentDateTime = new Date();
+    const timeDifference = currentDateTime - givenDateTime;
+
+    // Calculate the difference in days
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    // Calculate the difference in months
+    const givenMonth = givenDateTime.getMonth() + 1; // Months are 0-based
+    const currentMonth = currentDateTime.getMonth() + 1; // Months are 0-based
+    const monthsDifference = (currentDateTime.getFullYear() - givenDateTime.getFullYear()) * 12 + (currentMonth - givenMonth);
+
+    // Calculate the difference in years
+    const yearsDifference = currentDateTime.getFullYear() - givenDateTime.getFullYear();
+
+    if (yearsDifference > 0) {
+        return yearsDifference + " years"
+    } else if (monthsDifference > 0) {
+        return monthsDifference + " months"
+    } else {
+        return daysDifference + " days"
+    }
+
+}
+
+console.log(getDateDifference("2023-10-01"))
+
+fetch('./reviews.json')
+    .then(response => response.json())
+    .then(data => {
+        data.reverse().map(ele => {
+            const starImages = Array(5).fill("./assets/icons/blank-star.svg");
+
+            for (let i = 0; i < ele.rating; i++) {
+                starImages[i] = "./assets/icons/default-star.svg";
+            }
+
+            reviewsBody.innerHTML += `
+            <div class="card reviewsCard">
+                <div class="reveiewCardHeader flex-between">
+                <div class="star-rating flex-center">
+                ${starImages.map((src) => `<img src="${src}" alt="star" />`).join("")}
+                </div>
+
+                <p>${getDateDifference(ele.created_at)}</p>
+                </div>
+
+                <div class="reviewerInfo">
+                <div class="avatar flex-center"><span>${getNameInitials(ele.custName)}</span></div>
+                <h4 class="reviewerName">${ele.custName}</h4>
+                <img
+                    src="./assets/icons/blue-tick.svg"
+                    alt="Blue Tick"
+                    class="blue-tick"
+                />
+                </div>
+
+                <p>${ele.review}</p>
+
+                <div class="likes-dislikes">
+                    <img src="./assets/icons/arrow-up.svg" alt="arrow up" />
+                    <p>${ele.review_likes.length}</p>
+                    <img src="./assets/icons/arrow-down.svg" alt="arrow down" />
+                    <p>${ele.dislikes.length}</p>
+                </div>
+            </div>
+            `
+        })
+    })
+    .catch(error => {
+        console.error('Error loading JSON file:', error);
+    });
